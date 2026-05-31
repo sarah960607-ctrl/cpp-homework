7698d15861112bd08f97dc6438d0176a381aa3ec
banala <sarah960607@gmail.com>
2026-05-31 07:09:05 +0000

    10101 Bangla Numbers

---
+++ Problem/UVa_10101.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+void bangla(long long n)
+{
+    if (n >= 10000000)
+    {
+        bangla(n / 10000000);
+        cout << " kuti";
+        n %= 10000000;
+    }
+
+    if (n >= 100000)
+    {
+        cout << " " << n / 100000 << " lakh";
+        n %= 100000;
+    }
+
+    if (n >= 1000)
+    {
+        cout << " " << n / 1000 << " hajar";
+        n %= 1000;
+    }
+
+    if (n >= 100)
+    {
+        cout << " " << n / 100 << " shata";
+        n %= 100;
+    }
+
+    if (n > 0)
+    {
+        cout << " " << n;
+    }
+}
+
+int main()
+{
+    long long n;
+    int case_num = 1;
+
+    while (cin >> n)
+    {
+        printf("%4d.", case_num++);
+
+        if (n == 0)
+            cout << " 0";
+        else
+            bangla(n);
+
+        cout << endl;
+    }
+}
